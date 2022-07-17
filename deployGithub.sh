1da012ceb2833ef9e3f517784596f0968193f709
Nils Winter <nilswinter@pop-os.localdomain>
2022-07-18 00:49:03 +0200

    updated gh pages base path

--- deployGithub.sh
+++ deployGithub.sh
@@ -1,10 +1,10 @@
-# !/usr/bin/env sh
+#!/usr/bin/env sh
 
 # abort on errors
 set -e
 
 # build
-yarn build
+npm run build
 
 # navigate into the build output directory
 cd dist
@@ -13,13 +13,14 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
+git checkout -b main
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:NilsBWinter/vue-roulette.git master:gh-pages
+git push -f git@github.com:NilsBWinter/vue-roulette.git main:gh-pages
 
 cd -
