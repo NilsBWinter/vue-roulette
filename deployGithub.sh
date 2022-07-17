fc81c025ef25cad35cb6eb4d3fda0e3d3c997dcc
Nils Winter <nilswinter@pop-os.localdomain>
2022-07-18 01:24:24 +0200

    set git info

--- deployGithub.sh
+++ deployGithub.sh
@@ -15,10 +15,14 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+# git config user.name "GitHub Actions Bot"
+# git config user.email "<>"
+
 git init
 git checkout -b main
 git add -A
-git commit -m 'deploy'
+# git commit -m 'deploy'
+git -c user.name='GitHub Actions Bo' -c user.email='<>' commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
