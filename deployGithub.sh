ba1a9f0b36f5e4891bbf6349e4ccf7746e5ba0cc
Nils Winter <nilswinter@pop-os.localdomain>
2022-07-18 00:40:34 +0200

    update deploy script

--- deployGithub.sh
+++ deployGithub.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:NilsBWinter/vue-roulette.git main:gh-pages
+git push -f git@github.com:NilsBWinter/vue-roulette.git master:gh-pages
 
 cd -
